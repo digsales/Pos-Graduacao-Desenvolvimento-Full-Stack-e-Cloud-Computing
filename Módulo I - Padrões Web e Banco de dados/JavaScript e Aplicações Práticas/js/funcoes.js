c5ea98476a82f9a15d6b503af32563f2656d968f
Diogo Sales <diogodobu@gmail.com>
2024-10-21 10:56:58 -0300

    arquivo js com funcoes

---
+++ Módulo I - Padrões Web e Banco de dados/JavaScript e Aplicações Práticas/js/funcoes.js
@@ -0,0 +1,36 @@
+function somar() {
+  const n1 = Number(document.getElementById("n1").value);
+  const n2 = Number(document.getElementById("n2").value);
+
+  const soma = n1 + n2;
+
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `Resultado: ${soma}`;
+
+  // Outra Opção para realizar o passo acima:
+  // document.getElementById("resultado").innerHTML = `Resultado: ${soma}`;
+}
+function subtrair() {
+  const n1 = Number(document.getElementById("n1").value);
+  const n2 = Number(document.getElementById("n2").value);
+
+  const subtracao = n1 - n2;
+
+  document.getElementById("resultado").innerHTML = `Resultado: ${subtracao}`;
+}
+function multiplicar() {
+  const n1 = Number(document.getElementById("n1").value);
+  const n2 = Number(document.getElementById("n2").value);
+
+  const multiplicacao = n1 * n2;
+
+  document.getElementById("resultado").innerHTML = `Resultado: ${multiplicacao}`;
+}
+function dividir() {
+  const n1 = Number(document.getElementById("n1").value);
+  const n2 = Number(document.getElementById("n2").value);
+
+  const divisao = n1 / n2;
+
+  document.getElementById("resultado").innerHTML = `Resultado: ${divisao.toFixed()}`;
+}
