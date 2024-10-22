745d010327cf562970bcd56d844542e54133b734
Diogo Sales <diogodobu@gmail.com>
2024-10-22 09:15:04 -0300

    function exibir

--- Módulo I - Padrões Web e Banco de dados/JavaScript e Aplicações Práticas/js/jogoDaVelha.js
+++ Módulo I - Padrões Web e Banco de dados/JavaScript e Aplicações Práticas/js/jogoDaVelha.js
@@ -11,17 +11,34 @@ function iniciar() {
   aviso = document.getElementById("aviso");
   jogador = 1;
 
-  for (let i = 0; i < 3; i++) {
-    tabuleiro[i] = [];
-    for (let j = 0; j < 3; j++) {
-      tabuleiro[i][j] = 0;
+  for (let l = 0; l < 3; l++) {
+    tabuleiro[l] = [];
+    for (let c = 0; c < 3; c++) {
+      tabuleiro[l][c] = 0;
     }
   } // montar tabuleiro
 
   console.table(tabuleiro);
+  exibir();
 }
 
-function exibir() {}
+function exibir() {
+  let tabela = "<table border='1' cellpadding='10'>";
+
+  for (let l = 0; l < 3; l++) {
+    tabela += "<tr>";
+
+    for (let c = 0; c < 3; c++) {
+      tabela += `<td>${c + 1}, ${l + 1}</td>`;
+    }
+
+    tabela += "<tr>";
+  }
+
+  tabela += "</table>";
+
+  board.innerHTML = tabela;
+}
 
 function jogar() {}
 
