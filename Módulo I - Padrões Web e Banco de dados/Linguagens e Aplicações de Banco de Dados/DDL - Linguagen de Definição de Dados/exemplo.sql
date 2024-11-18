88bdff8d2aa9343e8838a797a28e8993d374ef90
Diogo Sales <diogodobu@gmail.com>
2024-11-18 12:26:42 -0300

    comentario

--- Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DDL - Linguagen de Definição de Dados/exemplo.sql
+++ Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DDL - Linguagen de Definição de Dados/exemplo.sql
@@ -10,6 +10,7 @@ CREATE TABLE cliente(
 ALTER TABLE cliente DROP estado;
 ALTER TABLE cliente ADD uf char(2);
 -- ALTER TABLE cliente RENAME COLUMN estadocivil TO estado_civil
+-- Função RENAME não está sendo reconhecida no VSCode
 ALTER TABLE cliente DROP estadocivil;
 ALTER TABLE cliente ADD estado_civil char(2);
 ALTER TABLE cliente ALTER COLUMN estado_civil char(1);
