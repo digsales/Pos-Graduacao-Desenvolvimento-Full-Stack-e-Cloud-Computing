baa1ce2187db9d1cb8d3eb007e74b73bac588c38
Diogo Sales <diogodobu@gmail.com>
2024-11-20 10:30:04 -0300

    comentario

--- Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DDL - Linguagen de Definição de Dados/delete.sql
+++ Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DDL - Linguagen de Definição de Dados/delete.sql
@@ -1,1 +1,5 @@
 DROP TABLE carro;
+-- Não exclui caso a tabela esteja vinculada a outra.
+
+-- DROP TABLE endereco CASCADE;
+-- CASCADE força a exlusão das tableas mesmo que ela possua relações, NÃO recomendado.
