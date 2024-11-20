ddd6eff9a3c44230cc3fb3027547926662fd4b9f
Diogo Sales <diogodobu@gmail.com>
2024-11-20 11:19:38 -0300

    FK

---
+++ Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DDL - Linguagen de Definição de Dados/fk.sql
@@ -0,0 +1,13 @@
+CREATE TABLE cliente (
+  codcliente INT PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE endereco (
+  codendereco INT PRIMARY KEY,
+  uf CHAR(2) NOT NULL,
+  cidade VARCHAR(50) NOT NULL,
+  bairro VARCHAR(50),
+  codcliente INT,
+  FOREIGN KEY (codcliente) REFERENCES cliente.codcliente
+)
