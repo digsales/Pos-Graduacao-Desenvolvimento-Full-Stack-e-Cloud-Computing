1ca4db27a9f67e46d7eb48b3bbc0fcc694253b2f
Diogo Sales <diogodobu@gmail.com>
2024-11-28 10:56:20 -0300

    comentarios

--- Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DML - Linguagem de Manipulação de Dados/insert.sql
+++ Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DML - Linguagem de Manipulação de Dados/insert.sql
@@ -1,3 +1,5 @@
+-- Primeira maneira
+
 INSERT INTO cliente (
   codcliente,
   nome,
@@ -14,6 +16,8 @@ INSERT INTO cliente (
   "C"
 );
 
+-- Maneira simplificada
+
 INSERT INTO cliente VALUES (
   1,
   "Ana Silva",
@@ -22,6 +26,8 @@ INSERT INTO cliente VALUES (
   "RJ",
   "C"
 );
+
+-- Os dados que não foram selecionados serão NULL
 
 INSERT INTO cliente (
   codcliente,
