41515e312fb9491238ca9dbcf28cfd1db12d484b
Diogo Sales <diogodobu@gmail.com>
2024-11-28 11:12:20 -0300

    update v2

--- Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DML - Linguagem de Manipulação de Dados/update.sql
+++ Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DML - Linguagem de Manipulação de Dados/update.sql
@@ -1,3 +1,8 @@
 UPDATE cliente
   SET cidade = "Teresina", uf = "PI", sexo = "M", estado_civil = "S"
   WHERE codcliente = 3;
+
+-- Limpar colunas em toda a tabela, usado para que os clientes atualizarem seus dados
+
+UPDATE cliente
+  SET cidade = Null, uf = NULL
