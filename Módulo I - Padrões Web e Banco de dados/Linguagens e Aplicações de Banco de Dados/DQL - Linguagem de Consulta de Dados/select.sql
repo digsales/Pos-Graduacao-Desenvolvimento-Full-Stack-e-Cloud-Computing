97a9e3267e9dd41ac9e5f0aa6adc8aeff3c72375
Diogo Sales <diogodobu@gmail.com>
2024-12-02 09:35:10 -0300

    usando where

--- Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DQL - Linguagem de Consulta de Dados/select.sql
+++ Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DQL - Linguagem de Consulta de Dados/select.sql
@@ -1,4 +1,11 @@
+-- Seleciona tudo da tabela cliente
 SELECT * FROM cliente;
 
+-- Seleciona a coluna nome da tabela cliente
 SELECT nome
   FROM cliente;
+
+-- Seleciona a coluna idade, da tabela cliente, onde o nome é igual a Lucas
+SELECT idade
+  FROM cliente
+  WHERE nome = "Lucas";
