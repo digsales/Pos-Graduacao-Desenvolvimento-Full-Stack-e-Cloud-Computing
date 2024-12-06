f75f91bf0c7368e9840ad5c69edfdc20204e73e5
Diogo Sales <diogodobu@gmail.com>
2024-12-06 08:57:55 -0300

    group by e having

---
+++ Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DQL - Linguagem de Consulta de Dados/funcoesDeAgregacao.sql
@@ -0,0 +1,5 @@
+-- GROUP BY agrupa dados de uma coluna com uma funcao de agregação, HAVING faz a filtragem. No caso está mostrando quantos cliente de cada sexo existe, desde que sexo não seja nulo
+SELECT sexo, COUNT(codcliente)
+  FROM cliente
+  GROUP BY sexo
+  HAVING sexo IS NOT NULL
