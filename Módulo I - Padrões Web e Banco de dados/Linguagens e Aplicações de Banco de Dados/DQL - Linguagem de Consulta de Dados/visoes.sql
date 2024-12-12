f3ee9955b1dccadae8a000fbe2e36449e547526a
Diogo Sales <diogodobu@gmail.com>
2024-12-12 10:15:13 -0300

    create or replace view

--- Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DQL - Linguagem de Consulta de Dados/visoes.sql
+++ Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DQL - Linguagem de Consulta de Dados/visoes.sql
@@ -12,3 +12,9 @@ SELECT * FROM vw_alunos_df
 
 -- Excluir VIEW
 DROP VIEW vw_alunos_df
+
+-- Atualizar uma VIEW
+CREATE OR REPLACE VIEW vw_alunos_df AS
+  SELECT nome
+    FROM aluno
+    WHERE uf="DF";
