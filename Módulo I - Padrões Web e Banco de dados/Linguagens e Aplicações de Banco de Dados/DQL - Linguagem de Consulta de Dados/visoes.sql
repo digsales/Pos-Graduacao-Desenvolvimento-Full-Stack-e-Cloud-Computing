f42f7cbeda354b1cc714ceb1a48b572b31fedd04
Diogo Sales <diogodobu@gmail.com>
2024-12-12 11:32:39 -0300

    comentario

--- Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DQL - Linguagem de Consulta de Dados/visoes.sql
+++ Módulo I - Padrões Web e Banco de dados/Linguagens e Aplicações de Banco de Dados/DQL - Linguagem de Consulta de Dados/visoes.sql
@@ -25,4 +25,6 @@ ALTER VIEW vw_alunos_df AS
     FROM aluno
     WHERE uf="DF";
 
+-- Melhor maneira de alterar e mais universal nas linguagens SQL é usar o DROP e depois o CREATE, assim você exclui e depois refaz da maneira desejada.
+
 -- Visões são apenas para visualização, não conseguimos inserir, alterar ou excluir dados.
